02d660bf9e7e6818b8193eac46e21622f7a93fd3
Ki113d <ki113d.69@gmail.com>
2012-12-21 15:58:58 +0100

    Updated and removed unused weechat configurations.

--- .weechat/python/test.py
+++
@@ -1,8 +0,0 @@
-import weechat
-
-def ktestCB(data, buffer, args):
-    weechat.prnt("", str(type(buffer)))
-    return weechat.WEECHAT_RC_OK
-
-weechat.register("test", "Ki113d", "lolz", "YO MUMMA", "", "", "")
-weechat.hook_command("ktest", "", "", "", "", "ktestCB", "")
